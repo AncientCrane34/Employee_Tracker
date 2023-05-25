fb83bf71496a8a85db4a8501e0d6d3882e8fa4c4
AncientCrane34 <erikscout@hotmail.com>
2023-05-24 19:31:37 -0500

    removed info

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection(
     {
       host: 'localhost',
       user: 'root',
-      password: 'fzA4?y#9em',
+      password: '',
       database: 'employees_db'
     },
     console.log(`Connected to the employees_db database.`)
